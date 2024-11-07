c94825052d537bf415ba6980ea7dd1fd15f3fdad
GuyWhoLovesFrogs <ni.muendermann@gmail.com>
2024-11-07 09:42:12 +0000

    martin ist toll

--- martin.py
+++ martin.py
@@ -1,4 +1,15 @@
+class Klasse2:    
+    def op2(self, i):
+        return i
+    
 class Klasse1:
+    def __init__(self, objekt: Klasse2):
+        self.objekt = objekt
     def op1(self):
         for i in range(0, 9):
-            op2(i)
+            print(self.objekt.op2(i))
+        
+objekt2 = Klasse2()
+objekt = Klasse1(objekt2)
+
+objekt.op1()        ^^^                     ^^^^^^^^^^^^^^^                                 ^^      qw
